455ce578ef95a896abf1813fd890f882d0950284
jw910731 <jw910731@gmail.com>
2022-07-08 15:07:46 +0800

    [Docs] Delete build dir from VCS

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ release = '1.0.0'
 # ones.
 extensions = [
     'myst_parser',
-    'sphinx_rtd_theme'
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
