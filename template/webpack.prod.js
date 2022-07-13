b95d04306ab71e0ac18016f48926567d694ef0ec
David Day <dj6082013@gmail.com>
2022-07-13 15:40:42 +0800

    [DevEnv] Add FileManager plugin.

--- template/webpack.prod.js
+++ template/webpack.prod.js
@@ -1,18 +1,20 @@
 const webpack = require("webpack");
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.yaml$/,
-        use: [{ loader: 'file-loader'}]
-      },
-    ]
-  },
   plugins: [
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            { source: './*.yaml', destination: './dist' },
+          ],
+        }
+      }
+    }),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1,
     }),
