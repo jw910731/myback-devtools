cb943d0a52825057122c2049ca20a0633cc2a8cb
jw910731 <jw910731@gmail.com>
2022-07-13 10:28:04 +0800

    [DevEnv] Import ReactDOM from client package

--- template/src/index.js
+++ template/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App.js";
 
 window.MYBACK_API_TOKEN ??= "DEVELOPMENT_API_TOKEN";
